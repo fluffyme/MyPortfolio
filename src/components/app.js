48ab2b1713c5ccdebe6f5870e2cff1c38c2bdf6e
fluffyme <analisnnic@gmail.com>
2019-06-01 23:51:19 +0300

    created the skeleton for the home page

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
-import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home.js";
+import About from "./pages/about.js";
+import Contact from "./pages/contact.js";
+import Blog from "./pages/blog.js";
+import PortfolioDetail from "./portfolio/portfolio-detail.js";
+import NoMatch from "./pages/no-match.js";
 
 export default class App extends Component {
   render() {
     return (
-      <div className="app">
-        <h1>My Portfolio</h1>
-        <h2>React Redux Router</h2>
-        <div>{moment().format("dddd")}</div>
+      <div className="container">
+        <Router>
+          <div>
+            <NavigationContainer />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route
+                exact
+                path="/portfolio/:slug"
+                component={PortfolioDetail}
+              />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
