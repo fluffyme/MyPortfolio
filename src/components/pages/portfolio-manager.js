1bc7c5a54b2275ae1e3ab5262a59682db3c10c36
fluffyme <analisnnic@gmail.com>
2019-06-27 00:05:18 +0300

    created portfolio form component

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 
 import PortfolioSideBarList from "../portfolio/portfolio-sidebar-list";
+import PortfolioForm from "../portfolio/portfolio-form";
 
 export default class PortfolioManager extends Component {
   constructor() {
@@ -10,8 +11,18 @@ export default class PortfolioManager extends Component {
     this.state = {
       portfolioItems: []
     };
+    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(
+      this
+    );
+    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
   }
 
+  handleSuccessfulFormSubmission(portfolioItem) {
+    //update the portfolio state and add the item to the list
+  }
+  handleFormSubmissionError(error) {
+    console.log("form error", error);
+  }
   getPortfolioItems() {
     axios
       .get("https://alisnic.devcamp.space/portfolio/portfolio_items", {
@@ -34,7 +45,10 @@ export default class PortfolioManager extends Component {
     return (
       <div className="portfolio-manager-wrapper">
         <div className="left-column">
-          <p>Portfolio Form</p>
+          <PortfolioForm
+            handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+            handleFormSubmissionError={this.handleFormSubmissionError}
+          />
         </div>
 
         <div className="right-column">
---
+++ src/components/portfolio/portfolio-form.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+
+export default class PortfolioForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      description: "",
+      category: "",
+      position: "",
+      url: "",
+      thumb_image: "",
+      banner_image: "",
+      logo: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  buildForm() {
+    let formData = new FormData();
+    formData.append("portfolio_item[name]", this.state.name);
+    formData.append("portfolio_item[description]", this.state.description);
+    formData.append("portfolio_item[url]", this.state.url);
+    formData.append("portfolio_item[category]", this.state.category);
+    formData.append("portfolio_item[position]", this.state.position);
+
+    return formData;
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+  handleSubmit(event) {
+    this.buildForm();
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input
+              type="text"
+              name="name"
+              placeholder="item name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+
+            <input
+              type="text"
+              name="url"
+              placeholder="item url"
+              value={this.state.url}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="position"
+              placeholder="item position"
+              value={this.state.position}
+              onChange={this.handleChange}
+            />
+
+            <input
+              type="text"
+              name="category"
+              placeholder="category"
+              value={this.state.category}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div>
+            <input
+              type="text"
+              name="description"
+              placeholder="description"
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <button type="submit">Save</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
